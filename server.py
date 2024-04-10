d5b3df6257a11c5e433fe011390d702c5d870461
Stanley Pierre <stanleypierre@Stanleys-MacBook-Air-200.local>
2024-04-10 00:37:16 -0400

    middle point fix

--- server.py
+++ server.py
@@ -17,68 +17,6 @@ model = tf.keras.models.load_model('roadseg.h5', compile = False)
 H = 256
 W = 256
 
-# model = tf.keras.models.load_model('./potatoes.h5')
-# IMAGE_SIZE = 256
-# BATCH_SIZE = 32
-
-# dataset = tf.keras.preprocessing.image_dataset_from_directory(
-#     "PlantVillage",
-#     seed=123,
-#     shuffle=True,
-#     image_size=(IMAGE_SIZE,IMAGE_SIZE),
-#     batch_size=BATCH_SIZE
-# )
-
-# class_names = dataset.class_names
-# print(class_names)
-
-# def load_image(filename):
-#     img = load_img(filename, target_size=(IMAGE_SIZE, IMAGE_SIZE))
-#     return img
-
-# Route for seeing a data
-@app.route('/data')
-async def get_time():
-
-	# Returning an api for showing in reactjs
-	return {
-		'Name':"geek", 
-		"Age":"22",
-		"programming":"python"
-		}
-
-# @app.route('/dummypredict')
-# async def predict():
-#     src = 'PlantVillage/Potato___Late_blight/0acdc2b2-0dde-4073-8542-6fca275ab974___RS_LB 4857.JPG'
-#     img = load_image(src);
-	
-#     img_array = tf.keras.preprocessing.image.img_to_array(img)
-#     img_array = tf.expand_dims(img_array, 0)
-
-#     predictions = model.predict(img_array)
-#     predicted_class = class_names[np.argmax(predictions[0])]
-#     actual_class = src.split("/")[1];
-#     confidence = round(100 * (np.max(predictions[0])), 2)
-#     print(f"Actual: {actual_class},\n Predicted: {predicted_class}.\n Confidence: {confidence}%")
-
-#     # img_array = tf.keras.preprocessing.image.img_to_array(img)
-#     # img_array = tf.expand_dims(img_array, 0)
-
-#     # # image = read_file_as_image(await file.read())
-#     # # img_batch = np.expand_dims(image, 0)
-#     # # predictions = MODEL.predict(img_batch)
-    
-#     # predictions = MODEL.predict(img_array)
-
-#     # predicted_class = CLASS_NAMES[np.argmax(predictions[0])]
-#     # confidence = np.max(predictions[0])
-
-# 	# Returning an api for showing in reactjs
-#     return {
-#         'class': predicted_class,
-#         'confidence': confidence
-#     }
-
 def read_image(path):
     try:
         img = Image.open(path)
@@ -95,18 +33,39 @@ def within_bounds(r, c):
 
 @app.route("/predict", methods=["POST"])
 def processReq():
-    # start = [20, 200]
-    # end = [220, 50]
-    form = request.form
-    # print(type(form["startX"]))
-    start = [255 - int(form["startY"]), int(form["startX"])]
-    end = [255 - int(form["endY"]), int(form["endX"])]
-    thresh = int(form["threshold"])
-    # print(request.form)
-
+    
     data = request.files["file"]
     data.save("img.jpg")
     img = read_image("img.jpg");
+
+    def get_image_dimensions(image_path):
+        with Image.open(image_path) as img:
+            width, height = img.size
+        return width, height
+
+    width, height = get_image_dimensions("img.jpg")
+    print(f"Image dimensions: {width} x {height}")
+
+    form = request.form
+    # print(type(form["startX"]))
+    # print("Middle Y: " + form["middleY"])
+    print(form["middleX"])
+    startY = int(round(256 * float(form["startY"]) / height))
+    startX = int(round(256 * float(form["startX"]) / width))
+    endY = int(round(256 * float(form["endY"]) / height))
+    endX = int(round(256 * float(form["endX"]) / width))
+    middleX = None
+    middleY = None
+    if(form["middleY"] != 'undefined'):
+        middleY = int(round(256 * float(form["middleY"]) / height))
+        middleX = int(round(256 * float(form["middleX"]) / width))
+    
+    start = [255 - startY, startX]
+    end = [255 - endY, endX]
+    middle = None
+    if(form["middleY"] != 'undefined'):
+         middle = [255 - middleY, middleX]
+    thresh = int(form["threshold"])
 
     img_array = tf.keras.preprocessing.image.img_to_array(img)
     img_array = tf.expand_dims(img_array, 0)
@@ -132,26 +91,36 @@ def processReq():
     im_bw = cv2.threshold(seg, thresh, 255, cv2.THRESH_BINARY)[1]
     # print(thresh)
 
-    
-
     print(im_bw.shape)
     cv2.imwrite('binary_image.png', im_bw)
     cv2.imwrite('256image.png', main)
     costs = np.where(im_bw, 1, 1000)
-    path, cost = route_through_array(costs, start=(start[0],start[1]), end=(end[0],end[1]), fully_connected=True)
+
+    split_index = -1
+    if(middle):
+        path1, cost1 = route_through_array(costs, start=(start[0],start[1]), end=(middle[0],middle[1]), fully_connected=True)
+        path2, cost2 = route_through_array(costs, start=(middle[0],middle[1]), end=(end[0],end[1]), fully_connected=True)
+        path = path1 + path2
+        split_index = len(path1)
+    else:
+         path, cost = route_through_array(costs, start=(start[0],start[1]), end=(end[0],end[1]), fully_connected=True)
     print(len(path))
     # print(cost)
     seg_color = [255,255,255]
     path_color = [255, 0, 0]
+    path2_color = [0, 255, 255]
     start_color = [0, 255, 0]
+    middle_color = [255, 255, 0]
     end_color = [0, 0, 255]
     
 
 
     color = np.array(seg_color, dtype='uint8')
-    masked_img = np.where(im_bw[...,None], color, main)
+    # masked_img = np.where(im_bw[...,None], color, main)
+    masked_img = main
     cv2.imwrite('maskoverlay.png', masked_img)
     deltas = [-1, 0], [1, 0], [0, 1], [0, -1], [-1, -1], [-1, 1], [1, 1], [1, -1]
+    i = 0
     for point in path:
         masked_img[point[0]][point[1]] = path_color
         
@@ -159,10 +128,14 @@ def processReq():
             r = point[0] + delta[0]
             c = point[1] + delta[1]
             if(within_bounds(r, c)):
-                masked_img[r][c] = path_color
+                masked_img[r][c] = path_color if i < split_index else path2_color
+        i += 1
 
     masked_img[start[0]][start[1]] = start_color
     masked_img[end[0]][end[1]] = end_color
+
+    if(middle):
+         masked_img[middle[0]][middle[1]] = middle_color
 
     for delta in deltas:
             sr = start[0] + delta[0]
@@ -173,6 +146,11 @@ def processReq():
                 masked_img[sr][sc] = start_color
             if(within_bounds(er, ec)):
                 masked_img[er][ec] = end_color
+            if(middle):
+                mr = middle[0] + delta[0]
+                mc = middle[1] + delta[1]
+                if(within_bounds(mr, mc)):
+                    masked_img[mr][mc] = middle_color
     
     cv2.imwrite('pathoverlay.png', masked_img)
     out = cv2.addWeighted(main, 0.8, masked_img, 0.4,0)
@@ -180,11 +158,7 @@ def processReq():
     print(masked_img.shape)
     print(masked_img[start[0]][start[1]])
 
-    # return {
-    #     'class': 0,
-    #     'confidence': 0,
-    # }
-    return send_file('result.png', mimetype='image/jpeg')
+    return send_file('pathoverlay.png', mimetype='image/jpeg')
 
 	
 # Running app
